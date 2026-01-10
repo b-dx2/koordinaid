89419144b1a8941f37aa4773cacfef9e7aa4d2e7
b-dx2 <gitlab.m@p4h2m.net>
2026-01-10 17:25:16 +0100

    small correction

--- src/components/questionnaire/MetaForm.tsx
+++ src/components/questionnaire/MetaForm.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Slider } from '@/components/ui/slider';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import type { NetworkData } from '@/types';
